6e49246ba917c55b6ebe22aacb4e61e398ff9e28
Carl Bolduc <contact@carlbolduc.com>
2018-09-02 22:37:07 -0400

    Add Emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,120 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq inhibit-startup-message t)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq visible-bell 1)
+(use-package dracula-theme
+             :ensure t
+             :config
+             (load-theme 'dracula t))
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(use-package flycheck
+             :ensure t
+             :config
+             (global-flycheck-mode))
+
+(use-package counsel
+             ;; ivy and swiper will be installed as dependencies
+             :ensure t
+             :config
+             (ivy-mode 1)
+             (global-set-key (kbd "C-s") 'swiper))
+
+(use-package projectile
+             :ensure t
+             :config
+             (projectile-mode +1)
+             (setq projectile-completion-system 'ivy)
+             (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(use-package company
+             :ensure t
+             :config
+             (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package magit
+  :ensure t)
+
+
+;; Python
+(use-package anaconda-mode
+             :ensure t
+             :config
+             (add-hook 'python-mode-hook 'anaconda-mode))
+(use-package company-anaconda
+             :ensure t
+             :config
+             (eval-after-load "company"
+               '(add-to-list 'company-backends 'company-anaconda)))
+
+;; JavaScript
+(use-package tide
+             :ensure t
+             ;; not sure if we need all this since I'll be coding in JS
+             :after (typescript-mode company flycheck)
+             :hook ((typescript-mode . tide-setup)
+                    (typescript-mode . tide-hl-identifier-mode)
+                    (before-save . tide-format-before-save)))
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+(use-package js2-mode
+             :ensure t
+             :config
+             (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+             (setq-default js2-basic-offset 2)
+             (add-hook 'js2-mode-hook #'setup-tide-mode))
+
+;; json
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
+
+;; Web
+(use-package web-mode
+             :ensure t
+             :config
+             (require 'web-mode)
+             (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+             (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode js2-mode tide company-anaconda anaconda-mode magit company projectile counsel flycheck dracula-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
